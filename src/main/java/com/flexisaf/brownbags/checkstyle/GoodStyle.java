13e7c34a42d5a6207f7dd13cba6520cc4a3a3b05
peter <fr33wayz@gmail.com>
2016-01-22 15:18:12 +0100

    added GoodStyle

---
+++ src/main/java/com/flexisaf/brownbags/checkstyle/GoodStyle.java
@@ -0,0 +1,11 @@
+package com.flexisaf.brownbags.checkstyle;
+
+/**
+ * Created by peter on 1/22/16.
+ */
+public class GoodStyle {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World am a good style");
+	}
+}
